340270bbea2b6be97af6a24dab82332de2b7bfeb
Nicholas Klos <nicholask.39@gmail.com>
2023-07-07 18:53:40 -0400

    Lab 7 COP3502

---
+++ ni303140_Lab7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int arr[], int n) {
+  int i, j, swaps;
+
+  for (i = 0; i < n - 1; i++) {
+    swaps = 0;
+    for (j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swaps++;
+      }
+    }
+    printf("Index %d: %d swaps\n", i + 1, swaps);
+  }
+}
+
+int main() {
+  int arr[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  bubbleSort(arr, n);
+
+  return 0;
+}
+
+
